b236f7ee8531be8254f50fd0589f790cd5760acd
nik2nik2006 <belnv@yandex.ru>
2024-08-13 14:47:56 +0300

    Закончил основную часть схемы Prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,6 +7,7 @@ export async function GET() {
     return NextResponse.json(users);
 }
 export async function POST(req: NextRequest) {
+
     const data = await req.json();
     const user = await prisma.user.create({
         data
