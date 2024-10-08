d1eaa0d6e01f228ac6c8d786ff058bd87568c947
nik2nik2006 <belnv@yandex.ru>
2024-10-08 20:12:21 +0300

    time 8.34.48

---
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,13 @@
+import {prisma} from "@/prisma/prisma-client";
+import {notFound} from "next/navigation";
+import {Container, ProductImage, Title} from "@/components/shared";
+import {GroupVariants} from "@/components/shared/group-variants";
+
+export default async function ProductModalPage({ params: {id} }: {params: {id: string}}) {
+
+    return (
+        <h1>12121222121</h1>
+    )
+}
+
+
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,6 @@ export default async function Home() {
         }
     })
 
-    console.log(categories);
     return <>
         <Container className='mt-10'>
             <Title text='Все пиццы' size='lg' className='font-extrabold' />
